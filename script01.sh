c254f4adcd19a5d5013dd83b7e21afff47689422
sreenugg <32235601+sreenugg@users.noreply.github.com>
2017-10-23 00:31:21 +0530

    Update script01.sh

--- script01.sh
+++ script01.sh
@@ -4,3 +4,5 @@ echo "This job was executed via github"
 uname -a;whoami
 hostname
 echo "Poll SCM feature enabled for this Jenkins Job."
+echo
+echo "Slack Notifications integrated with Jenkins Job"
